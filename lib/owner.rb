5980d538b31f3d16f85f134d2bd24bfa35858b83
Kelsey Low <kklowlow@gmail.com>
2019-02-24 22:45:25 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,42 @@
 class Owner
-  # code goes here
+  attr_accessor :name, :pets
+  attr_reader :species
+  
+  @@all = []
+  
+  def initialize(species)
+    @species = species
+    @@all << self
+    @pets = {fishes: [], cats: [], dogs: []}
+  end
+  
+  # Instance Methods
+  def say_species
+    "I am a #{@species}."
+  end
+  
+  def buy_fish(fish)
+    @pets[:fishes] << Fish.new(fish)
+  end 
+  
+  def buy_cat(cat)
+    @pets[:cats] << Cat.new(cat)
+  end
+
+  def buy_dog(dog)
+    @pets[:dogs] << Dog.new(dog)
+  end
+  
+  # Class Methods
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    self.all.size
+  end
+  
+  def self.reset_all
+    self.all.clear
+  end
 end
